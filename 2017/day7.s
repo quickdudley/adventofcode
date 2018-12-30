adf2ab70454a7b46e6243adac95095d30c7f463c
Jeremy List <quick.dudley@gmail.com>
2018-12-31 11:09:14 +1300

    Open and close the input file

--- 2017/day7.s
+++ 2017/day7.s
@@ -1,7 +1,19 @@
 .section .text
 .globl _start
 _start:
+  # Open input data file
+  lui a0, %hi(datafile)
+  addi a0, a0, %lo(datafile)
+  li a1, 0 # O_RDONLY
+  li a2, 0
+  li a7, 1024 # open
+  ecall
+  addi s2, a0, 0 # save file descriptor to s2
 
+  # Close input data file
+  addi a0, s2, 0
+  li a7, 57
+  ecall
 
   # exit
   li a0, 0
@@ -10,4 +22,7 @@ _start:
   li a3, 0
   li a7, 93
   ecall
+
 .section .rodata
+datafile:
+  .string "day7.txt"
