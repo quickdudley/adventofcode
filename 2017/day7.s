e1845686018d9593a5d324d7a171feb2ad74205d
Jeremy List <quick.dudley@gmail.com>
2018-12-31 14:20:40 +1300

    Iterate over the input file doing nothing.

--- 2017/day7.s
+++ 2017/day7.s
@@ -5,21 +5,36 @@
 .globl _start
 _start:
   # Open input data file
-  lui a0, %hi(datafile)
-  addi a0, a0, %lo(datafile)
-  li a1, 0 # O_RDONLY
-  li a2, 0
-  li a7, 1024 # open
+  li a0, -100 # AT_FDCWD
+  lui a1, %hi(datafile)
+  addi a1, a1, %lo(datafile)
+  li a2, 0 # O_RDONLY
+  li a3, 0
+  li a7, 56 # open
   ecall
   addi s2, a0, 0 # save file descriptor to s2
 
+readloop:
+  # Read 1024 bytes from the file
+  addi a0, s2, 0
+  lui a1, %hi(readbuf)
+  addi a1, a1, %lo(readbuf)
+  li a2, 1024
+  li a7, 63 # read
+  ecall
+
+  bge zero, a0, on_eof
+  # TODO parse the buffer
+  j readloop
+
   # Close input data file
+on_eof:
   addi a0, s2, 0
   li a7, 57
   ecall
 
   # exit
-  li a0, 0
+  addi a0, s2, 0
   li a1, 0
   li a2, 0
   li a3, 0
@@ -29,3 +44,12 @@ _start:
 .section .rodata
 datafile:
   .string "day7.txt"
+dfmt:
+  .string "%d\n"
+.section .data
+  .p2align 4, 0
+
+memroot:
+  .zero 1024
+readbuf:
+  .zero 1024
